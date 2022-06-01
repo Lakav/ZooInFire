aec3560a9182aebe526ea67d357fcfd983710178
JCAVANIER <jules.cavanier@ynov.com>
2022-06-01 22:40:01 +0200

    add class eagle

---
+++ aigle.cpp
@@ -0,0 +1,11 @@
+#include "animal.h"
+#include "aigle.h"
+
+Eagle::Eagle(){
+    eagleMaleOrFemale = false;
+    eagleHunger = false;
+    eaglePregnancy = false;
+    eagleLifeExectancy = 25;
+	eagleLoyalty = true;
+    eagleAge = 0;
+}
---
+++ aigle.h
@@ -0,0 +1,17 @@
+#ifndef AIGLE_H
+#define AIGLE_H
+
+class Eagle: public Animal{
+        private:
+        bool eagleMaleOrFemale;
+        bool eagleHunger;
+        int eaglePregnancy;
+        int eagleLifeExectancy;
+        bool eagleLoyalty;
+        int eagleAge;
+
+        public: 
+
+};
+
+#endif
