5d8edb97757ae5fbe787774f7e30c6907830faad
JCAVANIER <jules.cavanier@ynov.com>
2022-06-01 22:35:47 +0200

    add class chicken

---
+++ poule.cpp
@@ -0,0 +1,11 @@
+#include "animal.h"
+#include "poule.h"
+
+Chicken::Chicken(){
+    chickenMaleOrFemale = true;
+    chickenHunger = false;
+    chickenPregnancy = false;
+    chickenLifeExectancy = 15;
+	chickenLoyalty = false;
+    chickenAge = 0;
+}
---
+++ poule.h
@@ -0,0 +1,18 @@
+#ifndef POULE_H
+#define POULE_H
+
+class Chicken: public Animal{
+        private:
+        bool chickenMaleOrFemale;
+        bool chickenHunger;
+        int chickenPregnancy;
+        int chickenLifeExectancy;
+        bool chickenLoyalty;
+        int chickenAge;
+
+        public: 
+
+};
+
+
+#endif
