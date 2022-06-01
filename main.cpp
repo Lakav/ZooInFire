948d0cf4f84654739a646c4e50f023dc8c34641b
JCAVANIER <jules.cavanier@ynov.com>
2022-06-01 17:26:21 +0200

    class

--- main.cpp
+++ main.cpp
@@ -2,10 +2,56 @@
 #include <string>
 using namespace std;
 
+int money 80000
+
+class zoo {
+    public: 
+
+};
+class animal: public zoo {
+
+}
+class tigre: public animal{
+
+}
+class aigle: public animal{
+
+}
+class poule: public animal{
+
+}
+
+class habitat: public tigre{
+
+}
+class habitat: public aigle{
+    
+}
+class habitat: public poule{
+    
+}
+
 
 int main(){
 
+    //menu
+    cout << "Vous avez : " << money << "€"<< endl; 
+    //achats
+    //demander a quel animal est lié l'achat et faire des sous menu 
+    cout << 
+    //vente
+    //demander a quel animal est lié la vente 
 
+    //gestion 
+    //possibilité de faire déplacer un animal d'un enclo a un autre
+
+    //passer le mois
 
     return 0;
+}
+
+
+
+class male: publique tigre {
+    ...
 }
