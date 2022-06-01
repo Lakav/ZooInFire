fd50eac44749fb6810fbf7b61fd8de395da70066
JCAVANIER <jules.cavanier@ynov.com>
2022-06-01 22:24:53 +0200

    add class tiger

---
+++ tigre.cpp
@@ -0,0 +1,11 @@
+#include "animal.h"
+#include "tigre.h"
+
+Tiger::Tiger(){
+    tigerMaleOrFemale = true;
+    tigerHunger = false;
+    tigerPregnancy = false;
+    tigerLifeExectancy = 25;
+	tigerLoyalty = false;
+    tigerAge = 4;
+}
---
+++ tigre.h
@@ -0,0 +1,17 @@
+#ifndef TIGRE_H
+#define TIGRE_H
+
+class Tiger: public Animal{
+        private:
+        bool tigerMaleOrFemale;
+        bool tigerHunger;
+        int tigerPregnancy;
+        int tigerLifeExectancy;
+        bool tigerLoyalty;
+        int tigerAge;
+
+        public: 
+
+};
+
+#endif
