1f136cc494b4b60b9ac9b766385949bb0f275171
JCAVANIER <jules.cavanier@ynov.com>
2022-06-02 14:11:50 +0200

    add class habitat

---
+++ habitat.cpp
@@ -0,0 +1,7 @@
+#include "habitat.h"
+
+Habitat::Habitat(){
+    tigerMax = 2;
+    chickenMax = 10;
+    eagleMax = 4;
+}
---
+++ habitat.h
@@ -0,0 +1,13 @@
+#ifndef HABITAT_H
+#define HABITAT_H
+
+
+class Habitat: public Zoo{
+    private:
+        int tigerMax;
+        int chickenMax;
+        int eagleMax;
+
+};
+
+#endif
