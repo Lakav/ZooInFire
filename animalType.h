9e1338fccb014dd106fa51b504047845be99068c
julescavanier <jules.cavanier@ynov.com>
2022-06-12 21:47:15 +0200

    add AnimalType

---
+++ animalType.h
@@ -0,0 +1,6 @@
+enum class animalType
+{
+    TIGER,
+    CHICKEN,
+    EAGLE,
+};
--- habitat.h
+++ habitat.h
@@ -1,10 +1,12 @@
 #ifndef HABITAT_H
 #define HABITAT_H
-
+#include "animal.h"
+#include "animalType.h"
 
 class Habitat{
     private:
-    
+        iAnimal* tigerCage[10];
+
     public:
         Habitat();
 };
